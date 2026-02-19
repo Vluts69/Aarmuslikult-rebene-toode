6f26dd12d17d8b0ead2fe2ccbaa8fba634971b82
Vidrik Luts <vidrik.luts@tptlive.ee>
2026-02-19 10:19:00 +0200

    HBJHLBIP

--- sl.js
+++ sl.js
@@ -1,5 +1,22 @@
-async function getData() {
-  const response = await fetch('https://api.coingecko.com/api/v3/search?query=bitcoin');
-  const data = await response.json();
-  console.log(data);
-}
+
+const list1=document.querySelector("#databox");
+let list=document.querySelector("#databox");
+const url = "https://api.coingecko.com/api/v3/search?query=bitcoin";
+
+const getinfo =async()=>{
+
+  let response = await fetch(url);
+  console.log(response);
+
+  let data1= await response.json();
+  console.log(data1);
+
+  list=data1.coins;
+  console.log(list);
+  
+  
+
+  list1.textContent="PETS PISDETZ";
+ }
+
+getinfo();
